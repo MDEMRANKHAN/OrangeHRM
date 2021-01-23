2eae16ca19ccaa94cae1ac1154f18a20e2ab2228
hello <hello@DESKTOP-2S679H3.fios-router.home>
2021-01-23 01:15:51 -0500

    Adding new Code

---
+++ SeleniumViaJenkins/src/OrangeHRM/OrangeHRM.java
@@ -0,0 +1,25 @@
+package OrangeHRM;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrangeHRM {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin");
+	
+		
+
+	}
+
+}
